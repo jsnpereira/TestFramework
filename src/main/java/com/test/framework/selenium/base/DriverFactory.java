7ad324a52e38518806706e4c2c3ce6c16b4ec967
jsnpereira@gmail.com <jsnpereira@gmail.com>
2020-08-10 22:40:23 -0300

    Change selenium grid URL

--- src/main/java/com/test/framework/selenium/base/DriverFactory.java
+++ src/main/java/com/test/framework/selenium/base/DriverFactory.java
@@ -12,7 +12,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class DriverFactory extends Base {
-    private static String nodeSelenium = "http://172.18.0.2:4444/wd/hub";
+    private static String nodeSelenium = "http://172.19.0.3:4444/wd/hub";
     private static boolean local = false;
 
     public static void Initialize(BrowserType browserType) throws MalformedURLException {
